665d2eacccab3dbeb466cbf6afce25dd2d0d6e1a
Pablo Alcañiz <alcaniz.pablo@gmail.com>
2026-05-25 23:01:46 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import httpx
+# pyrefly: ignore [missing-import]
+from telegram import Update
+# pyrefly: ignore [missing-import]
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+
